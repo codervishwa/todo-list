3c7257519f330daf7eb9f2740ea4bc25acadb0d2
Vishwa <vishwanath1246@gmail.com>
2024-11-24 21:07:03 +0530

    setting categories

--- main.js
+++ main.js
@@ -20,4 +20,31 @@ newCategoryForm.addEventListener("submit", (e) => {
   if (isCategoryEmpty) {
     return console.log("Please enter a task");
   }
+  categories.push({
+    _id: Date.now().toString,
+    category: category,
+    color: getRandomColor(),
+  });
+  newCategoryInput.value = "";
+  saveAndRender();
 });
+
+// functions
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(
+    LOCAL_STORAGE_CATEGORIES_KEY,
+    JSON.stringify(categories)
+  );
+}
+
+// Helpers
+function getRandomColor() {
+  var hex = Math.round(Math.random() * 0xffffff).toString(16);
+  while (hex.length < 6) hex = "0" + hex;
+  return `#${hex}`;
+}
